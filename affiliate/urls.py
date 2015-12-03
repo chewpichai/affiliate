64b441ff498a3370395ecc741167498fce0f2a5d
chewpichai <chewpichai@hotmail.com>
2015-12-03 23:59:14 +0700

    add log in function.

--- affiliate/urls.py
+++ affiliate/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import include, url
+from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from .viewsets import *
-
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -13,4 +13,5 @@ router.register(r'customer-stats', CustomerStatViewSet)
 urlpatterns = [
   url(r'^v1-api/token/', obtain_auth_token, name='api-token'),
   url(r'^v1-api/', include(router.urls)),
+  url(r'^$', TemplateView.as_view(template_name='affiliate/index.html')),
 ]
